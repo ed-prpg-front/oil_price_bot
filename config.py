4b46de9bcf33e1c2edd33a968cf5fb469e0aa0a0
ed-prpg-front <77780283+ed-prpg-front@users.noreply.github.com>
2026-03-27 17:14:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import pytz;
+
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+#Адрес страницы с ценами
+
+TARGET_URL = "https://new.trade.tatneft.ru/promo/production/?format=for_bottling&shipping-method=Автотранспорт&shipment-basis=13";
+
+#Московский часовой пояс
+
+MOSCOW_TZ = pytz.timezone("Europe/Moscow");
+
+CHAT_ID_FILE = os.getenv("CHAT_ID_FILE", "chat_id.txt")
+PRICES_FILE = os.getenv("PRICES_FILE", "prices.json")
