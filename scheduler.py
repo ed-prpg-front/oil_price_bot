9395e83918835292e3328b44bf7b0922fe9a15fe
ed-prpg-front <77780283+ed-prpg-front@users.noreply.github.com>
2026-04-04 15:48:39 +0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -7,13 +7,13 @@ def setup_scheduler(application,loop):
 
     scheduler.add_job(
     morning_report_job,
-    trigger = CronTrigger(hour=8,minute=0,timezone=MOSCOW_TZ),
+    trigger = CronTrigger(hour=15,minute=54,timezone=MOSCOW_TZ),
     args=[application],
     id = 'morning_report'
         )
     scheduler.add_job(
         afternoon_check_job,
-        trigger=CronTrigger(hour=15,minute=45,timezone=MOSCOW_TZ),
+        trigger=CronTrigger(hour=20,minute=45,timezone=MOSCOW_TZ),
         args=[application],
         id='afternoon_check',
         )
