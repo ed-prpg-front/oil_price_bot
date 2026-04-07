e3d360ef98a496b50a99ee8ead38735dea4efdb9
ed-prpg-front <77780283+ed-prpg-front@users.noreply.github.com>
2026-04-07 18:26:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CRON_SECRET = os.getenv("CRON_SECRET", "default_secret_change_me")
 
 #Адрес страницы с ценами
 
-TARGET_URL = "https://new.trade.tatneft.ru/promo/api/v1/products-categories?ascending=true&order=name";
+#TARGET_URL = "https://new.trade.tatneft.ru/promo/api/v1/products-categories?ascending=true&order=name";
 
 #Московский часовой пояс
 
